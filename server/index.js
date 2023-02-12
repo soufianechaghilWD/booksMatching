6f39d797677f2c019f84120b20eb1d939b015242
soufiane chaghil <soufianechaghil@gmail.com>
2023-02-12 21:01:02 +0100

    adding port var

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
+const PORT = process.env.PORT || 4000;
+
 const start = () => {
-  console.log('App running on port 4000');
+  console.log(`App running on port ${PORT}`);
   mongoose
     .connect(process.env.DB_URL)
     .then(() => console.log('Connected to DB'))
     .catch((err) => console.error('Could not connect to DB: ', err.message));
 };
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   start();
 });
